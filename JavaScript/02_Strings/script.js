cd4fd211ca46a9fad7a3cba7db4d8bdec2840f5f
sena57 <126655518+sena57@users.noreply.github.com>
2023-03-08 09:20:57 +0100

    doing nr 2 of the task

--- JavaScript/02_Strings/script.js
+++ JavaScript/02_Strings/script.js
@@ -4,3 +4,8 @@ let b = 3.9;
     result = a / b;
 
 console.log("The result of " + a + " divided by " + b + " is " + result);
+
+let firstname = "Sena";
+let lastname = "Özcan";
+
+console.log("Ich heiße " + firstname +  " " + lastname);
