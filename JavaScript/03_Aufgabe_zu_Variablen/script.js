5d0ee00cf0b1474d7d4427b912156a72a16d31a8
sena57 <126655518+sena57@users.noreply.github.com>
2023-03-08 09:33:23 +0100

    begining the task

---
+++ JavaScript/03_Aufgabe_zu_Variablen/script.js
@@ -0,0 +1,10 @@
+// Erstelle eine Variable banana mit dem Wert "Banane"
+let banana = "Banane";
+
+let apple = "Apple";
+
+let bananaPricePerKilo = 2.14;
+let applePricePerKilo = 3.43;
+
+let appleWeightPerApple = 0.34;
+let bananaWeightPerBanana = 0.22;
