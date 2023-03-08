03c11cb58a4b29d39b624db0b6a7385c7fed3ff5
sena57 <126655518+sena57@users.noreply.github.com>
2023-03-08 08:59:28 +0100

    changing variables

--- JavaScript/01_Variablen/script.js
+++ JavaScript/01_Variablen/script.js
@@ -2,19 +2,23 @@ console.log("Hello");
 console.log("Tolle Sache!");
 
 // Variablen
-let a = 7;
-let b = 8;
-let c = 10;
+   // let a = 7;
+   // let b = 8;
+   // let c = 10;
 
 // Berechne folgendes und gib das Eregebnis über console.log aus
-console.log(a + b + c);
-console.log((a + b) / c);
-console.log(c -a + b);
-console.log(a + b + c * 3);
-console.log((a + b + c)/4);
+   // console.log(a + b + c);
+   // console.log((a + b) / c);
+   // console.log(c -a + b);
+   // console.log(a + b + c * 3);
+   // console.log((a + b + c)/4);
 
 // Ändere die Variablen auf folgende Werte
-// a = 3
-// b = a + 3
-// c = c -3
+    let a = 3
+    let b = a + 3
+    let c = c -3
+
 // gib die neuen Werte der Variablen aus
+    console.log(a);
+    console.log(b);
+    console.log(c);
