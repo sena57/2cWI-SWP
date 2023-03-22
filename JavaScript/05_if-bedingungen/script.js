836c6df153d765cb184465e77cfd86b8b5ea3593
sena57 <126655518+sena57@users.noreply.github.com>
2023-03-22 09:32:47 +0100

    starting task 1

---
+++ JavaScript/05_if-bedingungen/script.js
@@ -0,0 +1,21 @@
+let number = 23
+
+// Aufgabe 1
+// Wenn die Zahl kleiner ist als 20  gib aus "Mini"
+if (number<20) {
+    console.log("Mini");
+}
+
+// Wenn die Zahl zw. 20 und 50 ist gib aus "Medium"
+if (number<20) {
+    
+}
+// Wenn die Zahl größer als 50 ist gib aus "Large"
+if (number>50) {
+    console.log("Large");
+}
+
+// Aufgabe 2
+// Wenn die Zahl kleiner gleich 7 ist gib aus "Mini"
+// Wenn die Zahl zwischen 7 und 90 ist gib  aus "Medium"
+// Wenn die Zahl größer als 90 ist gib aus "Large"
