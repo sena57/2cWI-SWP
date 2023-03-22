66ca20bc7ceeae0db2889b6c5fe0670a511ebdb8
sena57 <126655518+sena57@users.noreply.github.com>
2023-03-22 08:10:42 +0100

    added comments

--- JavaScript/03_Aufgabe_zu_Variablen/script.js
+++ JavaScript/03_Aufgabe_zu_Variablen/script.js
@@ -1,10 +1,19 @@
 // Erstelle eine Variable banana mit dem Wert "Banane"
 let banana = "Banane";
-
+// Erstelle eine Variable apple mit dem Wert "Apple"
 let apple = "Apple";
 
+
+// Erstelle eine Variable bananaPricePerKilo mit dem Wert 2.14
 let bananaPricePerKilo = 2.14;
+// Erstelle eine Variable applePricePerKilo mit dem Wert 3.43
 let applePricePerKilo = 3.43;
 
+
+// Ein Apfel hat durchschnittlich ein Gewicht von 0.34 Kilo
 let appleWeightPerApple = 0.34;
+// Eine Banane hat durchschnittlich ein Gewicht von 0.22 Kilo
 let bananaWeightPerBanana = 0.22;
+
+
+// Gib folgendes aus: Anzahl Bananen/Äpfel pro Kilo, Preis pro Banane/Apfel
