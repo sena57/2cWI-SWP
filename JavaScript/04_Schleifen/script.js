dd9333c0f9088cce44d3e4a265c1c40cf6f9e4b1
sena57 <126655518+sena57@users.noreply.github.com>
2023-03-22 09:05:11 +0100

    added task 3

--- JavaScript/04_Schleifen/script.js
+++ JavaScript/04_Schleifen/script.js
@@ -9,7 +9,11 @@ for (count = 9; count<100 ; count++) {
 }
 
 // Zähle rückwärts von 10 bis 0
-// Zähle rückwärts von 1234 bis 1207
+for (count = 10; count>0 ; count--) {
+    console.log(count + " ");
+}
+
+// Zähle rückwärts von 1234 bis 1>207
 
 // Gib Deinen Namen genau 8 mal aus
 // Zähle von 0 auf 10, dann gib Deinen Namen aus, dann zähle von 10 auf 0
