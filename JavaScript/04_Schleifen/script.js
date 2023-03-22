02b769495b245351dba0431a053c8bf5e45df447
sena57 <126655518+sena57@users.noreply.github.com>
2023-03-22 08:54:28 +0100

    added comments

---
+++ JavaScript/04_Schleifen/script.js
@@ -0,0 +1,7 @@
+// Zähle von 7 bis 45
+// Zähle von 9 bis 99
+// Zähle rückwärts von 10 bis 0
+// Zähle rückwärts von 1234 bis 1207
+
+// Gib Deinen Namen genau 8 mal aus
+// Zähle von 0 auf 10, dann gib Deinen Namen aus, dann zähle von 10 auf 0
