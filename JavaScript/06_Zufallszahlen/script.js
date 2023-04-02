e09b45969bc49926cbeb0889dc5d12bfca96fc30
sena57 <126655518+sena57@users.noreply.github.com>
2023-04-02 12:55:31 +0200

    adding random Numbers and comments

---
+++ JavaScript/06_Zufallszahlen/script.js
@@ -0,0 +1,6 @@
+let randomNumber = Math.random() * 10; //mit 10 sagt man das eine Zufahlszahl zwischen 0 und 10 ausgeben werden soll
+let random = Math.floor(randomNumber); //mit Math.floor werden die Kommarstellen abgeschnitten
+
+console.log(random);
+
+// let randomNumver = Math.random() * 10 + 10; es kommen Zahlen zwischen 10 und 20
