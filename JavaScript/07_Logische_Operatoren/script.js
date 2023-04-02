f5566c73a5b0e16b47594009190110b154a6b910
sena57 <126655518+sena57@users.noreply.github.com>
2023-04-02 13:06:13 +0200

    adding random Numbers generator and if clause

---
+++ JavaScript/07_Logische_Operatoren/script.js
@@ -0,0 +1,12 @@
+/*let randomNumber = Math.random() * 100;
+let random = Math.floor(randomNumber);
+
+console.log(random);
+*/
+
+let number1 = 25;
+let number2 = 79; 
+
+if (number1<number2 && number1<50) {
+    console.log("Zahl 1 ist kleiner als Zahl 2 und Mini");
+}
