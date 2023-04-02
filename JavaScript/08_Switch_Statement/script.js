cb8e665edc9b2637edb2b4aebebaf12aac66d5ef
sena57 <126655518+sena57@users.noreply.github.com>
2023-04-02 13:40:05 +0200

    adding random Numbers generator and if clauses

---
+++ JavaScript/08_Switch_Statement/script.js
@@ -0,0 +1,28 @@
+let randomNumber = Math.random() * 5 + 5;
+let random = Math.floor(randomNumber);
+
+console.log(random);
+
+if (randomNumber === 10) {
+    console.log('Ten');
+}
+
+if (random === 9) {
+    console.log('Nine');
+}
+
+if (random === 8) {
+    console.log('Eight');
+}
+
+if (random === 7) {
+    console.log('Seven');
+}
+
+if (random === 6) {
+    console.log('Six');
+}
+
+if (random === 5) {
+    console.log('Five');
+}
