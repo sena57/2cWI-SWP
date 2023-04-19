3c350a32caf3b3c0b87cb58d495d4e17b4b69b39
sena57 <126655518+sena57@users.noreply.github.com>
2023-04-19 17:00:18 +0200

    subtract and multiply function

--- JavaScript/10_Funktionen/script.js
+++ JavaScript/10_Funktionen/script.js
@@ -7,3 +7,19 @@ function add(a,b) {
 
 let ergebnis1 = add(a,b);
 console.log(ergebnis1);
+
+
+function subtract(a,b){
+    return a - b;
+}
+
+let ergebnis2 = subtract(a,b);
+console.log(ergebnis2);
+
+
+function multiply(a,b) {
+    return a * b;
+}
+
+let ergebnis3 = multiply(a,b);
+console.log(ergebnis3);
