d0ff421b20b0034e48525998ab26fbd9dda18444
sena57 <126655518+sena57@users.noreply.github.com>
2023-04-26 09:24:50 +0200

    creating array, output array numbers

---
+++ JavaScript/11_Arrays/script.js
@@ -0,0 +1,16 @@
+let array = [4,1,2,3];
+
+array.push(17);
+array.push(199);
+
+//a
+for (let i=0; i<array.length; i++) {
+    console.log(array[i]);
+}
+
+//b
+let summe = "";
+for (let i2=0; i>array.length;i2++) {
+    summe += array[i];
+}
+console.log(summe);
